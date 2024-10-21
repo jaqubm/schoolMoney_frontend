bbab066f571c028e207e821a0bdd2d992780a0f5
jaqubm <jaqobgm@gmail.com>
2024-10-21 19:27:58 +0200

    Added 'use client' statement

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import {ModeToggle} from "@/components/mode-toggle";
 
 
 export default function Home() {
-  return (
+    // Use like normal React
+
+    return (
     <>
       <ModeToggle />
     </>
-  );
+    );
 }
