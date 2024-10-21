bee1074fafff48afe29adc831733ec86daaf63a3
jaqubm <jaqobgm@gmail.com>
2024-10-21 19:22:56 +0200

    Init Repo with shadcn/ui library

---
+++ app/api/status/index.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import {axiosInstance} from "@/app/api";
+
+export const getApiStatus = async () => {
+    try {
+        const { data } = await axiosInstance.get('/Api/Status')
+
+        return data
+    } catch (error) {
+        console.error(error)
+
+        return {
+            apiStatus: "Failed",
+            databaseConnectionStatus: "Failed"
+        }
+    }
+}
