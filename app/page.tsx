4fcd6eb8bf3a4018da41dcc6840a0902d717d148
jaqubm <jaqobgm@gmail.com>
2024-10-22 15:13:20 +0200

    Example

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import {ModeToggle} from "@/components/mode-toggle";
+import {getApiStatus} from "@/app/api/status";
 
 
 export default function Home() {
     // Use like normal React
+
+    console.log(getApiStatus());
 
     return (
     <>
